b1aff35a9aa4a987d35466633ccd580024c73164
isylsam <isylvestersam@gmail.com>
2026-03-18 15:32:08 +0100

    import logo

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,12 +1,12 @@
 import { Menu } from "lucide-react";
 import { Button } from "../ui/button";
+import Image from "next/image";
 
 
 const Header = () => {
   return (<header>
-    <div className="flex justify-between">
-      {/* <Image /> */}
-      <h3>Healthy Recipe Finder</h3>
+    <div className="flex justify-between items-center">
+      <Image src='/images/logo.svg' alt="Logo" width={250} height={12} />
 
       <div className="hidden lg:flex lg:justify-between">
         <nav>
@@ -17,7 +17,9 @@ const Header = () => {
         </Button>
       </div>
 
-      <Menu />
+      <Button className="bg-primary/40 px-3 py-5 rounded-sm">
+        <Menu />
+      </Button>
     </div>
   </header>);
 }
