466e5d322ed9974bd9a56ecc1ca169a0aa7ba07c
isylsam <isylvestersam@gmail.com>
2026-03-20 04:10:57 +0100

    trending grid 75%

--- components/landing-page/trending-now/TrendingSection.tsx
+++ components/landing-page/trending-now/TrendingSection.tsx
@@ -5,14 +5,16 @@ import { ArrowLeftCircle, ChevronLeft, ChevronRight } from "lucide-react"
 
 const TrendingSection = () => {
   return (
-    <div className="flex flex-col iems-center text-center mt-27">
-      <div className="mb-9">
-        <h3 className="text-3xl font-semibold mb-1">Trending Now</h3>
-        <p className="text-center text-card-foreground">When it comes to discovering new content, searching by genre can be a great way to find something that fits your preferences.</p>
-        <Button size='xl' className="mt-3">
-          <p>Find More</p>
-          <ChevronRight />
-        </Button>
+    <div className="flex flex-col iems-center text-center mt-27 lg:text-left lg:mt-32">
+      <div className="mb-9 flex flex-col lg:flex-row lg:justify-center">
+        <h3 className="text-3xl font-semibold mb-1 lg:w-full lg:text-5xl lg:text-left">Trending Now</h3>
+        <div>
+          <p className="text-card-foreground md:w-[70%] mx-auto lg:mx-0 lg:w-full">When it comes to discovering new content, searching by genre can be a great way to find something that fits your preferences.</p>
+          <Button size='xl' className="mt-3">
+            <p>Find More</p>
+            <ChevronRight />
+          </Button>
+        </div>
       </div>
       <TrendingGrid />
     </div>
